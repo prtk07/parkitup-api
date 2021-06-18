6a3f719fdae7f15931a9cdc9266ed136289e3687
Prateek Verma <prateekverma70@gmail.com>
2021-06-19 02:30:08 +0530

    routes added: /api/getuser /api/addvehicles /api/getvehicles

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const cors = require("cors");
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
 const { UserSchema, VehicleSchema } = require("./Schema.js");
-const { generateJWT } = require("./utils.js");
+const { generateJWT, getUser } = require("./utils.js");
+const { jwt } = require("jsonwebtoken");
 
 const port = 8080;
 
@@ -67,6 +68,64 @@ app.post("/api/login", (req, res) => {
                     res.send({ token: generateJWT(user), user });
                 } else res.send({ message: "incorrect password" });
             } else res.send({ message: "User Does not Exists!" });
+        })
+        .catch((e) => {
+            res.send({ message: "Something went wrong" });
+        });
+});
+
+app.get("/api/getuser", (req, res) => {
+    let auth = req.headers.authorization;
+    auth = auth.replace("Bearer ", "");
+    let username = getUser(auth);
+    User.findOne({ username })
+        .then((data) => {
+            if (data) {
+                let { name, username, mobile } = data;
+                res.send({ name, username, mobile });
+            } else res.send({ message: "User Not Found!!!" });
+        })
+        .catch((e) => {
+            if (e) res.send({ message: "Something not right!" });
+        });
+});
+
+app.post("/api/addvehicles", (req, res) => {
+    let token = req.headers.authorization;
+    let { vehiclenumber, type } = req.body;
+    let user = getUser(token);
+    Vehicle.findOne({ vehicleno: vehiclenumber })
+        .then((data) => {
+            if (!data) {
+                let newVehicle = new Vehicle({
+                    vehicleno: vehiclenumber,
+                    vehicletype: type,
+                    owner: user,
+                });
+                newVehicle
+                    .save()
+                    .then((data) => {
+                        res.send(data);
+                    })
+                    .catch((e) => {
+                        res.send({ message: "something went wrong" });
+                    });
+            } else res.send({ message: "Vehicle already exist" });
+        })
+        .catch((e) => {
+            console.log(e);
+            res.send({ message: "Vehicle already exist" });
+        });
+});
+
+app.get("/api/getvehicles", (req, res) => {
+    const token = req.headers.authorization;
+    let user = getUser(token);
+    Vehicle.find({ owner: user })
+        .then((data) => {
+            if (data.length > 0) {
+                res.send({ vehicles: data });
+            } else res.send({ message: "no vehicles for the user added" });
         })
         .catch((e) => {
             res.send({ message: "Something went wrong" });
